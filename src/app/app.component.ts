7a1bc451b2e525c44ed83a1c4d41adb8ff864338
Snoopy8403 <77445381+Snoopy8403@users.noreply.github.com>
2024-09-24 21:21:17 +0200

    templateBefejezve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'sablonMuveletek';
+  elemek = ['elso elem', 'masodik elem', 'harmadik elem'];
+
+  onSubmit(e: any) {
+    e.preventDefault();
+    const ujErtek = e.target.elements.cim.value;
+    this.elemek.push(ujErtek);
+    e.target.reset();
+  }
+
+  elemTorlese(id: number) {
+    this.elemek.splice(id, 1);
+  }
 }
